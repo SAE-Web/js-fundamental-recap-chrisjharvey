dd80f82f26ff1e21ecc9278d2116e90f49711ed4
Dom <dom@pilifter.com>
2020-02-03 15:31:51 +0000

    initial commit

---
+++ code.js
@@ -0,0 +1,13 @@
+const greeting = ['Introduction','to','Javascript'] 
+const numbers = [7,42,44,47,58,59,52]
+
+//1. Write code that will output the size of the variable greeting
+// and the variable numbers
+
+//2. Write code that will compare the size of these two variables and return the larger of the two
+
+//3a. Create code that will take the variable greeting and output "Welcome to an Introduction to Javascript"
+
+//3b. Create code that will that the variable numbers and out the number(s) that are even and a multiple of EITHER 13 OR 14
+
+//4. Write code that will check if 34 is is the variable numbers
