e6979e1c9e341e4e7b7a30228ac0375d61ad8ece
Chris Harvey <chris-harvey94@gmail.com>
2020-02-05 14:58:24 +0000

    first commit

--- code.js
+++ code.js
@@ -3,10 +3,19 @@ const numbers = [7,42,44,47,58,59,52]
 
 //1. Write code that will output the size of the variable greeting
 // and the variable numbers
-
+console.log(greeting.length);
+console.log(number.length);
 //2. Write code that will compare the size of these two variables and return the larger of the two
+if (greeting.length > numbers.length) {
+    console.log(greeting)
+} else {
+    console.log(numbers)
+    
+}
 
 //3a. Create code that will take the variable greeting and output "Welcome to an Introduction to Javascript"
+console.log(`Welcome to an ${greeting[0]} ${greeting[1]} ${greeting[2]}`);
+    
 
 //3b. Create code that will that the variable numbers and out the number(s) that are even and a multiple of EITHER 13 OR 14
 
